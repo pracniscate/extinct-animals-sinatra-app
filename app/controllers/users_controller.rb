e5fd88639494a522bf4bd296c540c85d8f211415
pracniscate <lorena.giddens@outlook.com>
2018-08-25 22:34:03 -0400

    submit the sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
 
     # renders sign up form if the user is not logged in
+    # if logged in, redirects to the main page of collected animals
     get '/signup' do
         if !logged_in?
             erb :'/users/create_user'
+        else
+            redirect '/show_animals'
+        end
+    end
+
+    # submits the sign up form
+    # if the input contains blank space, render the sign up form again
+    # if user signs up with their username, email, & password, save the params
+    # set their session to their user_id
+    # & render the main page containing a collection of animals added
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect '/signup'
+        else
+            @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+            @user.save
+            session[:user_id] = @user.id
+            redirect '/show_animals'
         end
     end
 
